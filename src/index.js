326042318d5e8a4f57e8bf470cf1d11aeffc113b
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-07-18 13:34:22 -0300

    feat(index): cria a saida

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Saída
+console.log("Digite o nome do seu jogador: ")
+
+let nickname = "Felipe"
+
+//declara uma variável
+console.log("Bem-vindo, " + nickname)
+
+//concatenando uma mensagem fixa + uma variável
+console.log(nickname + " entrou no servidor.")
+console.log(nickname + " entrou no servidor.")
