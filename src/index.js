c8f469f127b890cac5f20c99bb21859fd4797df4
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-07-30 14:23:09 -0300

    fix(index): atuazlia mensagem

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
+const notificacao = "Pokemon Go diz: "
+
 //Saída
-console.log("Digite o nome do seu jogador: ")
-
-let nickname = "Felipe"
-
-//declara uma variável
-console.log("Bem-vindo, " + nickname)
-
-//concatenando uma mensagem fixa + uma variável
-console.log(nickname + " entrou no servidor.")
-console.log(nickname + " entrou no servidor.")
+console.log(notificacao + "Tem um novo pokemon na região");
+console.log(notificacao + "Você foi derrotado");
