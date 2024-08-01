5e3e887d468231d582c1f6622991aa5ad4d3ec59
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-08-01 15:42:49 -0300

    aula 02 - criacao de let e const

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const notificacao = "Pokemon Go diz: "
+let poteCafe = "Café pilão";
+let poteAcucar = "Açucar cristal";
+let poteBiscoito = "Biscoito maizena";
 
-//Saída
-console.log(notificacao + "Tem um novo pokemon na região");
-console.log(notificacao + "Você foi derrotado");
+poteCafe = "Café 3 corações"
+
+console.log("Na cozinha da vovó hoje tem: " + poteCafe + ", " + poteAcucar + ", " + poteBiscoito)
